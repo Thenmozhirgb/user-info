c173c187c6aebd64e30d3fedfc2ecf10b874cf01
Thenmozhirgb <75113380+Thenmozhirgb@users.noreply.github.com>
2021-02-15 18:43:02 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import {Provider} from "react-redux";
 import configStore from "./redux/store/store";
 import "./index.css";
