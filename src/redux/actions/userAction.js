43d35d9f23c8728e1a5d0d5da1c91760f91c54ac
Thenmozhirgb <75113380+Thenmozhirgb@users.noreply.github.com>
2021-02-15 18:41:32 +0530

    userAction.js updated

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,6 +1,7 @@
 export const CREATE_USER = 'CREATE_USER';
 export const GET_USER_LIST ='GET_USER_LIST';
-
+export const SIGN_IN_SUCESS ='SIGN_IN_SUCESS';
+export const SIGN_OUT_SUCESS = 'SIGN_OUT_SUCESS';
 
 export const createUser = (params) => {
     return {type: CREATE_USER, payload: params}
@@ -10,3 +11,10 @@ export const getUserList =(params) => {
     return {type: GET_USER_LIST, payload: params}
 }
 
+export const signInSuccess =() => {
+    return {type: SIGN_IN_SUCESS}
+}
+
+export const signOutSuccess =() => {
+    return {type: SIGN_OUT_SUCESS}
+}
